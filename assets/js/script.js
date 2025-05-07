df9150426d7da583331570deff614d4972ae7cf4
Ankit kumar singh <singhsushant9691@gmail.com>
2025-05-08 00:03:02 +0530

    PROJECTS ADDED

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "web designing", "android development", "web development"],
+    strings: ["frontend development", "backend development", "Full stack web development", "Software devoloper", "DSA entthusiast", "competitive programmer"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
